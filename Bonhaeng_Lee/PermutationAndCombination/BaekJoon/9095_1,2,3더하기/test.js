49bf602271ebd9d4963b7fcc1458092cb4044cf3
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-01 21:43:55 +0900

    BOJ - 9095. 1,2,3 더하기(DFS)

--- Bonhaeng_Lee/PermutationAndCombination/BaekJoon/9095_1,2,3더하기/test.js
+++ Bonhaeng_Lee/PermutationAndCombination/BaekJoon/9095_1,2,3더하기/test.js
@@ -1,5 +1,36 @@
-const solution = function (s) {};
+const solution = function (s) {
+  const input = s.toString().trim().split("\n").map(Number);
+  input.shift();
+
+  let count = 0;
+  function dfs(sum, N) {
+    if (sum === N) {
+      count++;
+      return;
+    }
+    if (sum > N) return;
+    for (let i = 1; i <= 3; i++) {
+      dfs(sum + i, N);
+    }
+  }
+
+  const result = [];
+  for (let x of input) {
+    count = 0;
+    dfs(0, x);
+    result.push(count);
+  }
+  console.log(result.join("\n"));
+  return result.join("\n");
+};
 
 test("TC1", () => {
-  expect(solution("")).toStrictEqual("");
+  expect(
+    solution(`3
+4
+7
+10`)
+  ).toStrictEqual(`7
+44
+274`);
 });
