21b55162d718364087559313172bececab119f3f
zeromountain <sonyeongsan@gmail.com>
2022-01-02 18:54:24 +0900

    solved 9095

---
+++ Yeongsan_Son/PermutationAndCombination/9095.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const input = fs.readFileSync('./Yeongsan_Son/PermutationAndCombination/9095.txt').toString().split('\n').map(Number);
+let [range, ...numbers] = input;
+
+function sol(numbers) {
+  let count = 0;
+  // number 배열의 각 요소에 대한 경우의수
+  function DFS(sum, n) {
+    if (sum > n) return;
+    if (sum === n) {
+      count++;
+      return;
+    }
+    let op = 1;
+    while (op < 4) {
+      DFS(sum + op, n);
+      op++;
+    }
+  }
+
+  const answer = [];
+  numbers.forEach(num => {
+    count = 0;
+    DFS(0, num);
+    answer.push(count);
+  });
+
+  return answer.reduce((acc, curr) => {
+    acc = acc + curr + '\n';
+    return acc;
+  }, '');
+}
+
+console.log(sol(numbers));
