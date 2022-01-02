003c93e7d73d2cdbeea200737139f5aeda8c381b
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-03 00:00:04 +0900

    BOJ - 1182. 부분수열의 합(refactor)

--- Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1182_부분수열의합/test.js
+++ Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1182_부분수열의합/test.js
@@ -4,7 +4,6 @@ const solution = function (input) {
   const arr = input2.split(" ").map(Number);
 
   const visited = [...Array(arr.length)].fill(0);
-  let cnt = 0;
 
   const getSum = function () {
     let sum = 0;
@@ -14,11 +13,10 @@ const solution = function (input) {
     return sum;
   };
 
+  let cnt = 0;
   // s는 0일 때 sum이 0이면 cnt가 늘어나는 문제를 sum = null로 해결
   const dfs = function (L, sum = null) {
     if (sum === s) cnt++;
-
-    if (L == n) return;
     // L은 이전 인덱스의 요소를 다시 넣을 수 없도록 함 ( i + 1 전달 )
     for (let i = L; i < n; i++) {
       visited[i] = 1;
@@ -38,9 +36,9 @@ test("TC1", () => {
 -7 -3 -2 5 8`)
   ).toStrictEqual("1");
 });
-// test("TC2", () => {
-//   expect(
-//     solution(`2 -2
-// -1 -1`)
-//   ).toStrictEqual("1");
-// });
+test("TC2", () => {
+  expect(
+    solution(`2 -2
+-1 -1`)
+  ).toStrictEqual("1");
+});
