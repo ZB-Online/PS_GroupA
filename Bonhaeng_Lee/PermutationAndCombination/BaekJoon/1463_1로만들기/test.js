57985c478838546ef22ebc62d0fd80e6792cba5e
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-02 20:17:50 +0900

    BOJ - 1463. 1로 만들기(bottom-up - refactor)

--- Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1463_1로만들기/test.js
+++ Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1463_1로만들기/test.js
@@ -4,9 +4,11 @@ const solution = function (i) {
   const dp = Array(N + 1).fill(0);
 
   for (let i = 2; i <= N; i++) {
-    dp[i] = dp[i - 1] + 1;
-    if (i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-    if (i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    dp[i] = Math.min(
+      dp[i - 1] + 1,
+      i % 2 === 0 ? dp[i / 2] + 1 : Infinity,
+      i % 3 === 0 ? dp[i / 3] + 1 : Infinity
+    );
   }
   console.log(dp[N]);
   return dp[N] + "";
