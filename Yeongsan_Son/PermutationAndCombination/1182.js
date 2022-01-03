8caec7a75a46f571379a4a637573ff5408f5ebea
zeromountain <sonyeongsan@gmail.com>
2022-01-03 22:01:23 +0900

    solved 1182

---
+++ Yeongsan_Son/PermutationAndCombination/1182.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const input = fs.readFileSync('./Yeongsan_Son/PermutationAndCombination/1182.txt').toString().trim().split('\n');
+const s = +input[0].split(' ')[1];
+const arr = input[1].split(' ').map(Number);
+
+// arr의 원소를 가지고 만들 수 있는 모든 부분집합에서
+// 부분집합의 합이 s와 같은 경우
+
+function sol(arr, s) {
+  const equaled = [];
+  const temp = [];
+
+  function DFS(level) {
+    let sum;
+    if (level === arr.length) {
+      sum = temp.reduce((acc, curr) => acc + curr, 0);
+      if (sum === s) equaled.push(temp.slice());
+      return;
+    }
+    temp.push(arr[level]);
+    DFS(level + 1);
+    temp.pop();
+    DFS(level + 1);
+  }
+
+  DFS(0);
+
+  return equaled.filter(el => el.length !== 0).length;
+}
+
+console.log(sol(arr, s));
+// sol(arr, s);
