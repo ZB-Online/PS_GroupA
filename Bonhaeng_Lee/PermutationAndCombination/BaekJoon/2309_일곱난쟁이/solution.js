56f888118d094e59393654929bf9b84ac1b0f91d
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-04 02:23:30 +0900

    BOJ - 2309. 일곱 난쟁이

--- Bonhaeng_Lee/PermutationAndCombination/BaekJoon/2309_일곱난쟁이/solution.js
+++ Bonhaeng_Lee/PermutationAndCombination/BaekJoon/2309_일곱난쟁이/solution.js
@@ -1,5 +1,42 @@
-const solution = function (s) {};
+const solution = function (input) {
+  const arr = input.toString().trim().split('\n').map(Number);
+  const total = arr.reduce((sum, height) => sum + height, 0);
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (total - (arr[i] + arr[j]) === 100) {
+        // BOJ 제출
+        console.log(
+          arr
+            .filter((_, idx) => idx !== i && idx !== j)
+            .sort((heightA, heightB) => heightA - heightB)
+            .join('\n')
+        );
+        return arr
+          .filter((_, idx) => idx !== i && idx !== j)
+          .sort((heightA, heightB) => heightA - heightB)
+          .join('\n');
+      }
+    }
+  }
+};
 
 test('TC1', () => {
-  expect(solution('')).toStrictEqual('');
+  expect(
+    solution(`20
+7
+23
+19
+10
+15
+25
+8
+13`)
+  ).toStrictEqual(`7
+8
+10
+13
+19
+20
+23`);
 });
