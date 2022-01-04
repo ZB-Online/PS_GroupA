53fc8ab5cb49a12937bb450c25115bc78560f6c3
zeromountain <sonyeongsan@gmail.com>
2022-01-04 16:56:26 +0900

    jest 적용

--- Yeongsan_Son/PermutationAndCombination/BaekJoon/1463.js
+++ Yeongsan_Son/PermutationAndCombination/BaekJoon/1463.js
@@ -1,8 +1,8 @@
 // X % 3 === 0 => X / 3
 // X % 2 === 0 => X / 2
 // -1
-const fs = require('fs');
-const X = +fs.readFileSync('./Yeongsan_Son/PermutationAndCombination/1463.txt').toString();
+// const fs = require('fs');
+// const X = +fs.readFileSync('./Yeongsan_Son/PermutationAndCombination/1463.txt').toString();
 
 function sol(num) {
   let min = Number.MAX_SAFE_INTEGER;
@@ -22,4 +22,10 @@ function sol(num) {
   return min;
 }
 
-console.log(sol(X));
+test('TC1', () => {
+  expect(sol(2)).toStrictEqual(1);
+});
+
+test('TC2', () => {
+  expect(sol(10)).toStrictEqual(3);
+});
---
+++ Yeongsan_Son/PermutationAndCombination/BaekJoon/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  testMatch: ['**/*.[jt]s?(x)'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+};
