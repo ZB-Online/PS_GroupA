33eab3e510abe659f909ed0818ebca954b6b1ec5
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-04 23:39:09 +0900

    Fix: ESLint autoformatting 적용(@babel/eslint-parser의 no babel config file detected,,, 에러 해결)

--- Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1759_암호만들기.js
+++ Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1759_암호만들기.js
@@ -1,5 +1,44 @@
-const solution = function (s) {};
+const solution = function (i) {
+  const [input1, input2] = i.toString().trim().split('\n');
+  const [L, C] = input1.split(' ').map(Number);
+  const arr = input2.split(' ');
+  console.log(L, C, arr);
+  const visited = new Array(L).fill(false);
+  // const alpabet = 'abcdefghijklmnopqrstuvwxyz';
+
+  const validateCode = str => {
+    // 알파벳 "모음"이 2개 이상인지 검사하는 regex
+    const m = str.match(/[aeiou]/gi);
+    return m === null ? 0 : m.length;
+  };
+
+  const combination = (L, temp) => {
+    if (validateCode(temp)) return;
+    for (let i = L; i < C; i++) {
+      visited[i] = true;
+      combination(i + 1, (temp += arr[i]));
+      visited[i] = false;
+    }
+  };
+  combination(0, '');
+};
 
 test('TC1', () => {
-  expect(solution('')).toStrictEqual('');
+  expect(
+    solution(`4 6
+a t c i s w`)
+  ).toStrictEqual(`acis
+acit
+aciw
+acst
+acsw
+actw
+aist
+aisw
+aitw
+astw
+cist
+cisw
+citw
+istw`);
 });
