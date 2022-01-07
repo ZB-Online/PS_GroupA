041dcef1e0229552969829063ba8a131edaebd1b
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-07 22:43:27 +0900

    BOJ - 1799. 비숍(refactor3 - 검은색, 흰색 칸에 놓을 때 유효한지 판단하는 규칙 함수 분리)

--- Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1799_비숍.js
+++ Bonhaeng_Lee/PermutationAndCombination/BaekJoon/1799_비숍.js
@@ -1,45 +1,40 @@
 const solution = function (i) {
   const [SIZE, ...arr] = i.toString().trim().split('\n');
-  const board = arr.map(v => v.split(' ').map(Number));
-  const bishop = [...Array(10)].map(() => [...Array(10).fill(false)]);
-  const possibleArea = [];
-  for (let i = 0; i < SIZE; i++) {
-    for (let j = 0; j < SIZE; j++) {
-      if (board[i][j]) possibleArea.push([i, j]);
-    }
-  }
+  const board = []; // 비숍을 놓을 수 있는 좌표의 "일차원 배열"
+  arr.forEach((row, i) =>
+    row
+      .split(' ')
+      .forEach((col, j) => (col ? +col && board.push([i, j]) : +col))
+  );
+  const bishop = [...Array(10)].map(() => [...Array(10).fill(false)]); // 비숍을 놓은 좌표의 "이차원 배열"
 
   function isPossible(x, y) {
-    // 왼쪽 위 대각선
     let nx = x - 1;
     let ny = y - 1;
     while (nx >= 0 && ny >= 0) {
-      if (bishop[nx][ny]) return false;
-      nx--;
-      ny--;
+      if (bishop[nx--][ny--]) return false;
     }
     // 오른쪽 위 대각선
     nx = x - 1;
     ny = y + 1;
     while (nx >= 0 && ny < SIZE) {
-      if (bishop[nx][ny]) return false;
-      nx--;
-      ny++;
+      if (bishop[nx--][ny++]) return false;
     }
-
     return true;
   }
 
+  const isEven = index => index % 2 === 0;
+  const isOdd = index => index % 2 === 1;
+
   let ansBlack = 0;
-  let ansWhite = 0;
-  function dfsBlack(index, cnt) {
+  const dfsBlack = (index, cnt) => {
     ansBlack = Math.max(ansBlack, cnt);
-    for (let i = index; i < possibleArea.length; i++) {
-      const x = possibleArea[i][0];
-      const y = possibleArea[i][1];
-      // '짝수 행'인 경우 → 열의 인덱스 짝수
-      // '홀수 행'인 경우 → 열의 인덱스 홀수
-      if (!(x % 2 === 0 && y % 2 !== 0) && !(x % 2 === 1 && y % 2 !== 1)) {
+
+    for (let i = index; i < board.length; i++) {
+      const [x, y] = board[i];
+      // "짝수" 행인 경우 "짝수" 열이여야만 비숍을 놓을 수 있다.
+      // "홀수" 행인 경우 "홀수" 열이여야만 비숍을 놓을 수 있다.
+      if ((isEven(x) && isEven(y)) || (isOdd(x) && isOdd(y))) {
         if (!bishop[x][y] && isPossible(x, y)) {
           bishop[x][y] = true;
           dfsBlack(i + 1, cnt + 1);
@@ -47,16 +42,17 @@ const solution = function (i) {
         }
       }
     }
-  }
+  };
 
-  function dfsWhite(index, cnt) {
+  let ansWhite = 0;
+  const dfsWhite = (index, cnt) => {
     ansWhite = Math.max(ansWhite, cnt);
-    for (let i = index; i < possibleArea.length; i++) {
-      const x = possibleArea[i][0];
-      const y = possibleArea[i][1];
-      // '짝수 행'인 경우 → 열의 인덱스 홀수
-      // '홀수 행'인 경우 → 열의 인덱스 짝수
-      if (!(x % 2 === 0 && y % 2 !== 1) && !(x % 2 === 1 && y % 2 !== 0)) {
+
+    for (let i = index; i < board.length; i++) {
+      const [x, y] = board[i];
+      // "짝수" 행인 경우 "홀수" 열이여야만 비숍을 놓을 수 있다.
+      // "홀수" 행인 경우 "짝수" 열이여야만 비숍을 놓을 수 있다.
+      if ((isEven(x) && isOdd(y)) || (isOdd(x) && isEven(y))) {
         if (!bishop[x][y] && isPossible(x, y)) {
           bishop[x][y] = true;
           dfsWhite(i + 1, cnt + 1);
@@ -64,7 +60,7 @@ const solution = function (i) {
         }
       }
     }
-  }
+  };
 
   dfsBlack(0, 0);
   dfsWhite(0, 0);
