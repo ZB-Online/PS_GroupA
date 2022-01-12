afe99dd6002bccb0605f25b64a21d03d4bd58629
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-12 12:44:06 +0900

    Solve: BOJ - 1248. 맞춰봐

--- Bonhaeng_Lee/BruteForce_Recursion/BaekJoon/1248_맞춰봐.js
+++ Bonhaeng_Lee/BruteForce_Recursion/BaekJoon/1248_맞춰봐.js
@@ -1,41 +1,47 @@
-// sol1. DFS
+// sol1. Backtracking
 const solution = function (i) {
   const [N, signs] = i
     .toString()
     .trim()
     .split('\n')
     .map((input, i) => (i === 0 ? +input : input));
-  const cmp = [...Array(N)].map(() => [...Array(N)].fill(0));
-  const arr = [...Array(21)];
-  let isFound = false;
+  const matrix = [...Array(N)].map(() => [...Array(N)].fill(0));
+  const result = [...Array(N)];
 
-  const isPromising = (cnt, sum) => {
-    for (let i = 0; i <= cnt; i++) {
-      // cmp[0][cnt]~cmp[cnt][cnt]까지 열 1개 확인
-      if (cmp[i][cnt] === '+' && sum <= 0) return false;
-      if (cmp[i][cnt] === '-' && sum >= 0) return false;
-      if (cmp[i][cnt] === '0' && sum !== 0) return false;
-      sum -= arr[i];
+  const isPromising = idx => {
+    // 0행부터 찾으려는 idx까지
+    for (let i = 0; i <= idx; i++) {
+      let sum = 0;
+      // i행에 따라 각 열의 합을 구한다.
+      for (let j = i; j <= idx; j++) {
+        sum += result[j];
+        // 부호가 다르면 유효하지 않음
+        if (matrix[i][j] !== (sum === 0 ? '0' : sum > 0 ? '+' : '-'))
+          return false;
+      }
     }
     return true;
   };
 
-  const backtracking = (cnt, sum) => {
-    if (cnt === N) {
-      isFound = true;
-      return;
+  const backtracking = idx => {
+    if (idx === N) {
+      console.log(result.join(' '));
+      // BOJ 제출 : backtacking의 남은 콜스택을 무시하고 원하는 값만 출력한 뒤 solution() 탈출
+      process.exit();
     }
-    for (let i = -10; i <= 10 && !isFound; i++) {
-      arr[cnt] = i; // arr 배열에 i 입력
-      if (isPromising(cnt, sum + i)) backtracking(cnt + 1, sum + i);
+    for (let i = -10; i <= 10; i++) {
+      result[idx] = i; // result 배열에 i 입력
+      if (isPromising(idx)) backtracking(idx + 1);
     }
   };
 
+  // 부호 배열로 행렬 초기화
   let pos = 0;
   for (let i = 0; i < N; i++) {
-    for (let j = i; j < N; j++) cmp[i][j] = signs[pos++];
+    for (let j = i; j < N; j++) matrix[i][j] = signs[pos++];
   }
-  backtracking(0, 0);
+
+  backtracking(0);
 };
 
 test('TC1', () => {
