c099a5789e050009d4e1726f1b4aab00935d26bb
ysh2987 <ysh2987@naver.com>
2022-01-13 22:46:56 +0900

    18290

--- Songhyeon_Yu/BruteForce_NM/18290.js
+++ Songhyeon_Yu/BruteForce_NM/18290.js
@@ -6,41 +6,41 @@ const input = fs
   .split("\n")
   .map((v) => v.split(" ").map(Number));
 const [N, M, K] = input[0];
-const visited = Array(N).fill(Array(M).fill(false));
+const visited = Array.from({ length: N }, () => Array(M).fill(false));
 
 let arr = input.splice(1);
-let result = 0;
-const dirX = [0, 0, 1, -1];
-const dirY = [1, -1, 0, 0];
+let result = Number.MIN_SAFE_INTEGER;
+const dirX = [1, 0, -1, 0];
+const dirY = [0, 1, 0, -1];
 
-const solution = (sum, cnt, x, y, test) => {
+const solution = (sum, cnt, x, y) => {
   if (cnt === K) {
     result = Math.max(result, sum);
-    console.log(test);
     return;
   }
 
   for (let i = x; i < N; i++) {
-    let c2 = 0;
-    if (i == x) c2 = y;
-    for (let j = c2; j < M; j++) {
+    for (let j = y; j < M; j++) {
       if (visited[i][j]) continue;
+      // check
       let check = true;
       for (let q = 0; q < 4; q++) {
         let nx = i + dirX[q];
         let ny = j + dirY[q];
         if (0 <= nx && nx < N && 0 <= ny && ny < M && visited[nx][ny]) {
           check = false;
+          break;
         }
       }
-
+      // -----
       if (check) {
         visited[i][j] = true;
-        solution(sum + arr[i][j], cnt + 1, i, j, [...test, arr[i][j]]);
+        solution(sum + arr[i][j], cnt + 1, x, y);
         visited[i][j] = false;
       }
     }
   }
 };
 
-solution(0, 0, 0, 0, []);
+solution(0, 0, 0, 0);
+console.log(result);
