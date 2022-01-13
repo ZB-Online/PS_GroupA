59a2ae1d89e8fe788a7f3f89e4f780e596c0a2e2
Serzhul <wilshere917@gmail.com>
2022-01-13 12:18:27 +0900

    15650 solved

--- Daewon_Seo/BruteForce_NM/BaekJoon/15650.js
+++ Daewon_Seo/BruteForce_NM/BaekJoon/15650.js
@@ -1,7 +1,24 @@
 const solution = function (i) {
-    const [N, arr] = i
-        .toString()
-        .trim()
-        .split('\n')
-        .map((input, i) => (i === 0 ? +input : input));
+  const [N, M] = i
+    .toString()
+    .trim()
+    .split(" ")
+    .map((el) => +el);
+
+  const nums = Array.from({ length: N }, (_, i) => i + 1);
+
+  const getComb = (arr, pick, prefix = []) => {
+    if (pick === 0) {
+      console.log(prefix.join(" "));
+      return [prefix];
+    }
+
+    return arr.flatMap((v, i) =>
+      getComb(arr.slice(i + 1), pick - 1, [...prefix, v])
+    );
+  };
+
+  getComb(nums, M);
 };
+
+solution(`3 1`);
