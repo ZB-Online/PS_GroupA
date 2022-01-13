7d660ef42f8b2eb072be69dd31f5ccb11c84bf4d
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-14 03:05:22 +0900

    Solve: BOJ - 15654. N과 M (5)

--- Bonhaeng_Lee/BruteForce_NM/BaekJoon/15654_NM5.js
+++ Bonhaeng_Lee/BruteForce_NM/BaekJoon/15654_NM5.js
@@ -1,15 +1,84 @@
-// sol1. DFS
 const solution = function (i) {
-  const [N, arr] = i
+  const [[N, M], elements] = i
     .toString()
     .trim()
     .split('\n')
-    .map((input, i) => (i === 0 ? +input : input));
+    .map(input => input.split(' ').map(Number));
+  const result = [];
+  const perm = [];
+
+  const sortedElements = elements.sort((a, b) => a - b);
+  const DFS = () => {
+    if (perm.length === M) {
+      result.push(perm.join(' '));
+      return;
+    }
+    for (let i = 0; i < N; i++) {
+      if (!perm.includes(sortedElements[i])) {
+        perm.push(sortedElements[i]);
+        DFS();
+        perm.pop();
+      }
+    }
+  };
+
+  DFS();
+  // BOJ 제출
+  console.log(result.join('\n'));
+  return result.join('\n');
 };
 
 test('TC1', () => {
   expect(
-    solution(`4
--+0++++--+`)
-  ).toStrictEqual(`-2 5 -3 1`);
+    solution(`3 1
+4 5 2`)
+  ).toStrictEqual(`2
+4
+5`);
+});
+test('TC2', () => {
+  expect(
+    solution(`4 2
+9 8 7 1`)
+  ).toStrictEqual(`1 7
+1 8
+1 9
+7 1
+7 8
+7 9
+8 1
+8 7
+8 9
+9 1
+9 7
+9 8`);
+});
+test('TC3', () => {
+  expect(
+    solution(`4 4
+1231 1232 1233 1234`)
+  ).toStrictEqual(`1231 1232 1233 1234
+1231 1232 1234 1233
+1231 1233 1232 1234
+1231 1233 1234 1232
+1231 1234 1232 1233
+1231 1234 1233 1232
+1232 1231 1233 1234
+1232 1231 1234 1233
+1232 1233 1231 1234
+1232 1233 1234 1231
+1232 1234 1231 1233
+1232 1234 1233 1231
+1233 1231 1232 1234
+1233 1231 1234 1232
+1233 1232 1231 1234
+1233 1232 1234 1231
+1233 1234 1231 1232
+1233 1234 1232 1231
+1234 1231 1232 1233
+1234 1231 1233 1232
+1234 1232 1231 1233
+1234 1232 1233 1231
+1234 1233 1231 1232
+1234 1233 1232 1231`);
 });
