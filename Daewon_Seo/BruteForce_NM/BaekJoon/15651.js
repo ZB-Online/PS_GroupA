c914c156e8a8a94bc2d87ad612b31eabff6e62e4
Serzhul <wilshere917@gmail.com>
2022-01-13 12:23:19 +0900

    15651 solved

--- Daewon_Seo/BruteForce_NM/BaekJoon/15651.js
+++ Daewon_Seo/BruteForce_NM/BaekJoon/15651.js
@@ -1,7 +1,28 @@
 const solution = function (i) {
-    const [N, arr] = i
-        .toString()
-        .trim()
-        .split('\n')
-        .map((input, i) => (i === 0 ? +input : input));
+  const [N, M] = i
+    .toString()
+    .trim()
+    .split(" ")
+    .map((el) => +el);
+
+  const nums = Array.from({ length: N }, (_, i) => i + 1);
+
+  const permutation = [];
+  let res = "";
+  const dfs = (cnt) => {
+    if (cnt === M) {
+      res += `${permutation.join(" ")}\n`;
+      return;
+    }
+    for (let i = 0; i < N; i++) {
+      permutation.push(nums[i]);
+      dfs(cnt + 1);
+      permutation.pop();
+    }
+  };
+
+  dfs(0);
+  console.log(res.trim());
 };
+
+solution(`3 3`);
