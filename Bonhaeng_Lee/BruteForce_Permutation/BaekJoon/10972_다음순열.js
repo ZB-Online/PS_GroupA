354d83ec2c3019a599f9b94c09d605a4c3ef4aab
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-16 02:45:22 +0900

    Refactor: BOJ - 10972 다음 순열

--- Bonhaeng_Lee/BruteForce_Permutation/BaekJoon/10972_다음순열.js
+++ Bonhaeng_Lee/BruteForce_Permutation/BaekJoon/10972_다음순열.js
@@ -4,9 +4,7 @@ const solution = function (i) {
     .trim()
     .split('\n')
     .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
-
   let pivot = -1;
-
   // 맨 뒤에서부터 시작해서 오름차순이 끊기는 부분을 찾는다.
   for (let i = givenPerm.length - 1; i > 0; i--) {
     if (givenPerm[i - 1] < givenPerm[i]) {
@@ -20,27 +18,22 @@ const solution = function (i) {
     return -1;
   }
 
-  // pivot 보다 더 큰 값을 가진 swapB 찾기
   let min = givenPerm[pivot + 1];
-  let swapB = pivot + 1;
-  if (swapB + 1 < N) {
-    for (let i = pivot + 2; i < givenPerm.length; i++) {
-      if (givenPerm[i] < min && givenPerm[i] > givenPerm[pivot]) {
-        min = givenPerm[i];
-        swapB = i;
-      }
+  let minIdx = pivot + 1;
+  for (let i = pivot + 2; minIdx < N - 1, i < N; i++) {
+    const cur = givenPerm[i];
+    if (givenPerm[pivot] < cur && cur < min) {
+      min = cur;
+      minIdx = i;
     }
   }
 
   // no Optimization. O(n^2)
 
   // Optimization1. Worst O(nlogn), sort() - 120ms
-  // pivot과 이보다 더 큰 값을 가진 swapB를 교환한다.
-  [givenPerm[pivot], givenPerm[swapB]] = [givenPerm[swapB], givenPerm[pivot]];
-  console.log('37', pivot + 1, givenPerm.slice(pivot + 1, N));
+  [givenPerm[pivot], givenPerm[minIdx]] = [givenPerm[minIdx], givenPerm[pivot]];
   const answer = [
     ...givenPerm.slice(0, pivot + 1),
-    // 최적화 sort 대신 in-place?
     ...givenPerm.slice(pivot + 1, N).sort((a, b) => a - b),
   ].join(' ');
   console.log(answer);
@@ -49,7 +42,7 @@ const solution = function (i) {
   // Optimization2. O(n), in-place - 152ms
   // let K = N - 1;
   // let idx = pivot + 1;
-  // [givenPerm[pivot], givenPerm[swapB]] = [givenPerm[swapB], givenPerm[pivot]];
+  // [givenPerm[pivot], givenPerm[minIdx]] = [givenPerm[minIdx], givenPerm[pivot]];
   // while (idx < K) {
   //   [givenPerm[idx], givenPerm[K]] = [givenPerm[K], givenPerm[idx]];
   //   ++idx;
@@ -59,22 +52,29 @@ const solution = function (i) {
   // console.log(givenPerm.join(' '));
   // return givenPerm.join(' ');
 };
-
-// test('TC1', () => {
-//   expect(
-//     solution(`4
-// 1 2 3 4`)
-//   ).toStrictEqual(`1 2 4 3`);
-// });
-// test('TC2', () => {
-//   expect(
-//     solution(`5
-// 5 4 3 2 1`)
-//   ).toStrictEqual(-1);
-// });
-test('TC3', () => {
-  expect(
-    solution(`3
-2 3 1`)
-  ).toStrictEqual('3 1 2');
+describe('다음순열', () => {
+  //   it('TC1', () => {
+  //     expect(
+  //       solution(`4
+  // 1 2 3 4`)
+  //     ).toStrictEqual(`1 2 4 3`);
+  //   });
+  //   it('TC2', () => {
+  //     expect(
+  //       solution(`5
+  // 5 4 3 2 1`)
+  //     ).toStrictEqual(-1);
+  //   });
+  //   it('TC3', () => {
+  //     expect(
+  //       solution(`3
+  // 2 3 1`)
+  //     ).toStrictEqual('3 1 2');
+  //   });
+  it('TC4', () => {
+    expect(
+      solution(`6
+5 1 5 3 2 2`)
+    ).toStrictEqual('5 2 1 2 3 5');
+  });
 });
