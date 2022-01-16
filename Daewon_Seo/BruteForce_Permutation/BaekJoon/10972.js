99ee5f52671aa43dee38c87f3e5cc266da97335a
Serzhul <wilshere917@gmail.com>
2022-01-16 17:03:59 +0900

    10972 solved

--- Daewon_Seo/BruteForce_Permutation/BaekJoon/10972.js
+++ Daewon_Seo/BruteForce_Permutation/BaekJoon/10972.js
@@ -1,9 +1,43 @@
 const solution = (input) => {
-  const [N, M] = input
-    .toString()
-    .trim()
-    .split(" ")
-    .map((el) => +el);
+    let [N, target] = input
+        .toString()
+        .trim()
+        .split('\n')
+        .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
+
+    const swap = (i, j) => {
+        [target[i], target[j]] = [target[j], target[i]];
+    };
+
+    const reverse = (idx) => {
+        let start = idx,
+            end = N - 1;
+
+        while (start < end) {
+            swap(start, end);
+            start++;
+            end--;
+        }
+    };
+
+    const nextLargeIdx = (idx) => {
+        for (let i = N - 1; i > idx; i--) {
+            if (target[i] > target[idx]) return i;
+        }
+    };
+
+    for (let i = N - 1; i >= 0; i--) {
+        if (target[i] < target[i + 1]) {
+            const large = nextLargeIdx(i);
+            swap(i, large);
+            reverse(i + 1);
+            console.log(target.join(' '));
+            return;
+        }
+    }
+
+    console.log(-1);
 };
 
-solution();
+solution(`4
+1 2 3 4`);
