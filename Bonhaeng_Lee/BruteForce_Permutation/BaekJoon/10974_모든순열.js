dabf0f2472160c1c407b03428c102a16555c252b
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-16 11:23:53 +0900

    Solve - BOJ - 10974. 모든 순열

--- Bonhaeng_Lee/BruteForce_Permutation/BaekJoon/10974_모든순열.js
+++ Bonhaeng_Lee/BruteForce_Permutation/BaekJoon/10974_모든순열.js
@@ -1,14 +1,34 @@
 const solution = function (i) {
-  const [N, signs] = i
-    .toString()
-    .trim()
-    .split('\n')
-    .map((input, i) => (i === 0 ? +input : input));
+  const n = i.toString().trim();
+  let result = '';
+  const visit = [...Array(10)].fill(0);
+  const perm = [];
+
+  const permutations = () => {
+    if (perm.length === +n) {
+      result += perm.join(' ') + '\n';
+      return;
+    }
+    for (let i = 1; i <= +n; i++) {
+      if (!visit[i]) {
+        visit[i] = true;
+        perm.push(i);
+        permutations();
+        perm.pop();
+        visit[i] = false;
+      }
+    }
+  };
+  permutations();
+  console.log(result.substring(0, result.length - 1));
+  return result.substring(0, result.length - 1);
 };
 
 test('TC1', () => {
-  expect(
-    solution(`4
--+0++++--+`)
-  ).toStrictEqual(`-2 5 -3 1`);
+  expect(solution(`3`)).toStrictEqual(`1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1`);
 });
