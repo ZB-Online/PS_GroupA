8bbde95a95048e4d310ee0dfb783be9913022dda
Serzhul <wilshere917@gmail.com>
2022-01-17 20:38:18 +0900

    10819 solved

--- Daewon_Seo/BruteForce_Permutation/BaekJoon/10819.js
+++ Daewon_Seo/BruteForce_Permutation/BaekJoon/10819.js
@@ -1,9 +1,45 @@
 const solution = (input) => {
-  const [N, M] = input
-    .toString()
-    .trim()
-    .split(" ")
-    .map((el) => +el);
+    const [N, nums] = input
+        .toString()
+        .trim()
+        .split('\n')
+        .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
+
+    const checked = Array.from({ length: N }, () => false);
+
+    let max = 0;
+
+    let res = [];
+
+    const getSum = (arr) => {
+        let sum = 0;
+
+        for (let i = 0; i < arr.length - 1; i++) {
+            sum += Math.abs(arr[i] - arr[i + 1]);
+        }
+        return sum;
+    };
+
+    const dfs = (cnt) => {
+        if (cnt === N) {
+            max = Math.max(getSum(res), max);
+            return;
+        }
+
+        for (let i = 0; i < N; i++) {
+            if (!checked[i]) {
+                res.push(nums[i]);
+                checked[i] = true;
+                dfs(cnt + 1);
+                res.pop();
+                checked[i] = false;
+            }
+        }
+    };
+
+    dfs(0);
+    console.log(max);
 };
 
-solution();
+solution(`6
+20 1 15 8 4 10`);
