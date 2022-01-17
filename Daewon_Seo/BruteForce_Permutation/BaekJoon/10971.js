e457260005d0ceaf399626145844a54df91830b2
Serzhul <wilshere917@gmail.com>
2022-01-17 20:28:06 +0900

    10971 solved

--- Daewon_Seo/BruteForce_Permutation/BaekJoon/10971.js
+++ Daewon_Seo/BruteForce_Permutation/BaekJoon/10971.js
@@ -1,9 +1,46 @@
 const solution = (input) => {
-  const [N, M] = input
-    .toString()
-    .trim()
-    .split(" ")
-    .map((el) => +el);
+    const [N, ...grid] = input
+        .toString()
+        .trim()
+        .split('\n')
+        .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
+
+    const cities = Array.from({ length: N }, (_, i) => i);
+    const visited = Array.from({ length: N }, () => false);
+
+    let min = Number.MAX_SAFE_INTEGER;
+    let sum = 0;
+    const res = [];
+
+    const dfs = (cnt) => {
+        if (cnt === N) {
+            const cost = grid[res[N - 1]][res[0]];
+            if (cost !== 0) min = Math.min(min, sum + cost);
+            return;
+        }
+
+        for (let i = 0; i < N; i++) {
+            if (res.length > 0 && grid[res[res.length - 1]][cities[i]] === 0)
+                continue;
+            if (!visited[i]) {
+                sum += !res.length ? 0 : grid[res[res.length - 1]][cities[i]];
+                res.push(cities[i]);
+                visited[i] = true;
+                dfs(cnt + 1);
+                res.pop();
+                sum -= !res.length ? 0 : grid[res[res.length - 1]][cities[i]];
+                visited[i] = false;
+            }
+        }
+    };
+
+    dfs(0);
+    console.log(min);
 };
 
-solution();
+solution(`4
+0 10 15 20
+5 0 9 10
+6 13 0 12
+8 8 9 0
+`);
--- Daewon_Seo/BruteForce_Permutation/BaekJoon/10974.js
+++ Daewon_Seo/BruteForce_Permutation/BaekJoon/10974.js
@@ -1,37 +1,46 @@
 const solution = (input) => {
-    const [N] = input
+    const [N, ...grid] = input
         .toString()
         .trim()
         .split('\n')
         .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
 
-    const nums = Array.from({ length: N }, (_, i) => i + 1);
-    const checked = Array.from({ length: N }, () => false);
+    const cities = Array.from({ length: N }, (_, i) => i);
+    const visited = Array.from({ length: N }, () => false);
 
-    let res = '';
-
-    let permutation = [];
+    let min = Number.MAX_SAFE_INTEGER;
+    let sum = 0;
+    const res = [];
 
     const dfs = (cnt) => {
         if (cnt === N) {
-            res += `${permutation.join(' ')}\n`;
+            min = Math.min(min, sum + grid[res[N - 1]][res[0]]);
             return;
         }
 
         for (let i = 0; i < N; i++) {
-            if (!checked[i]) {
-                permutation.push(nums[i]);
-                checked[i] = true;
+            let [from, to] = !res.length
+                ? [0, cities[i]]
+                : [res.length - 1, cities[i]];
+
+            if (!visited[i]) {
+                sum += grid[from][to];
+                res.push(cities[i]);
+                visited[i] = true;
                 dfs(cnt + 1);
-                permutation.pop();
-                checked[i] = false;
+                res.pop();
+                sum -= grid[from][to];
+                visited[i] = false;
             }
         }
     };
 
     dfs(0);
-
-    console.log(res);
+    console.log(min);
 };
 
-solution(3);
+solution(`4
+0 10 15 20
+5 0 9 10
+6 13 0 12
+8 8 9 0`);
