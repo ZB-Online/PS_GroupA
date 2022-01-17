ee1f3b027b37df749d4318fd1f6a6386913295ec
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-17 11:40:05 +0900

    Solve - BOJ - 10819. 차이를 최대로

--- Bonhaeng_Lee/BruteForce_Permutation/BaekJoon/10819_차이를최대로.js
+++ Bonhaeng_Lee/BruteForce_Permutation/BaekJoon/10819_차이를최대로.js
@@ -1,14 +1,41 @@
 const solution = function (i) {
-  const [N, signs] = i
-    .toString()
-    .trim()
-    .split('\n')
-    .map((input, i) => (i === 0 ? +input : input));
+  const [N, ...A] = i.toString().trim().split(/\s/).map(Number);
+  const visit = [...Array(N)].fill(false);
+
+  const getSum = arr => {
+    let sum = 0;
+    for (let i = 0; i < N - 1; i++) {
+      sum += Math.abs(arr[i] - arr[i + 1]);
+    }
+    return sum;
+  };
+
+  let max = Number.MIN_SAFE_INTEGER;
+  const permutation = (perm, L) => {
+    if (L === N) {
+      max = Math.max(max, getSum(perm));
+      return;
+    }
+    for (let i = 0; i < N; i++) {
+      if (!visit[i]) {
+        visit[i] = true;
+        perm[L] = A[i];
+        permutation(perm, L + 1);
+        visit[i] = false;
+      }
+    }
+  };
+
+  permutation([], 0);
+  console.log(max);
+  return max;
 };
 
-test('TC1', () => {
-  expect(
-    solution(`4
--+0++++--+`)
-  ).toStrictEqual(`-2 5 -3 1`);
+describe('차이를최대로', () => {
+  it('TC1', () => {
+    expect(
+      solution(`6
+20 1 15 8 4 10`)
+    ).toStrictEqual(62);
+  });
 });
