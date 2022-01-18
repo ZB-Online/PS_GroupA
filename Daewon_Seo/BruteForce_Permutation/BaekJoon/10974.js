dd480c86a5cf26ae7385d5a04f8ad61212829446
Serzhul <wilshere917@gmail.com>
2022-01-18 13:49:18 +0900

    problems setting

--- Daewon_Seo/BruteForce_Permutation/BaekJoon/10974.js
+++ Daewon_Seo/BruteForce_Permutation/BaekJoon/10974.js
@@ -1,46 +1,34 @@
-const solution = (input) => {
-    const [N, ...grid] = input
-        .toString()
-        .trim()
-        .split('\n')
-        .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
+const [N] = require('fs')
+    .readFileSync('/dev/stdin')
+    .toString()
+    .trim()
+    .split('\n')
+    .map((input, i) => (i === 0 ? +input : input.split(' ').map(Number)));
 
-    const cities = Array.from({ length: N }, (_, i) => i);
-    const visited = Array.from({ length: N }, () => false);
+const nums = Array.from({ length: N }, (_, i) => i + 1);
+const checked = Array.from({ length: N }, () => false);
 
-    let min = Number.MAX_SAFE_INTEGER;
-    let sum = 0;
-    const res = [];
+let res = '';
 
-    const dfs = (cnt) => {
-        if (cnt === N) {
-            min = Math.min(min, sum + grid[res[N - 1]][res[0]]);
-            return;
+let permutation = [];
+
+const dfs = (cnt) => {
+    if (cnt === N) {
+        res += `${permutation.join(' ')}\n`;
+        return;
+    }
+
+    for (let i = 0; i < N; i++) {
+        if (!checked[i]) {
+            permutation.push(nums[i]);
+            checked[i] = true;
+            dfs(cnt + 1);
+            permutation.pop();
+            checked[i] = false;
         }
-
-        for (let i = 0; i < N; i++) {
-            let [from, to] = !res.length
-                ? [0, cities[i]]
-                : [res.length - 1, cities[i]];
-
-            if (!visited[i]) {
-                sum += grid[from][to];
-                res.push(cities[i]);
-                visited[i] = true;
-                dfs(cnt + 1);
-                res.pop();
-                sum -= grid[from][to];
-                visited[i] = false;
-            }
-        }
-    };
-
-    dfs(0);
-    console.log(min);
+    }
 };
 
-solution(`4
-0 10 15 20
-5 0 9 10
-6 13 0 12
-8 8 9 0`);
+dfs(0);
+
+console.log(res);
