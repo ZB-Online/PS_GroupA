dee4cb631b7d599d3299e4aeb0aa61b0dad3cb2d
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 01:33:17 +0900

    Solve: Math - BOJ - 10430 나머지

---
+++ Bonhaeng_Lee/Math/BaekJoon/1978_소수찾기.js
@@ -0,0 +1,18 @@
+const solution = function (i) {
+  const [A, B, C] = i.toString().trim().split(' ').map(Number);
+  const A_C = A % C;
+  const B_C = B % C;
+  console.log((A + B) % C);
+  console.log((A_C + B_C) % C);
+  console.log((A * B) % C);
+  console.log((A_C * B_C) % C);
+};
+
+describe('소수찾기', () => {
+  it('TC1', () => {
+    expect(solution(`5 8 4`)).toStrictEqual(`1
+1
+0
+0`);
+  });
+});
