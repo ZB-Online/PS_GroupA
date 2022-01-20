51bfcd04fcebe8ef9fb17d928ad53d476e061013
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 03:27:44 +0900

    Solve: Math - BOJ - 4375 1

--- Bonhaeng_Lee/Math/BaekJoon/4375_1.js
+++ Bonhaeng_Lee/Math/BaekJoon/4375_1.js
@@ -1,18 +1,34 @@
 const solution = function (i) {
-  const [A, B, C] = i.toString().trim().split(' ').map(Number);
-  const A_C = A % C;
-  const B_C = B % C;
-  console.log((A + B) % C);
-  console.log((A_C + B_C) % C);
-  console.log((A * B) % C);
-  console.log((A_C * B_C) % C);
+  const inputs = i.toString().trim().split('\n').map(Number);
+  let res = '';
+
+  for (const input of inputs) {
+    let multiples = '1';
+    // modular 연산으로 자릿수를 줄이면서 연산한다면 자릿수 누적 변수가 필요하다.
+    let size = 1;
+    while (true) {
+      if (multiples % input === 0) {
+        res += `${size}\n`;
+        break;
+      }
+      size++;
+      multiples += '1';
+      multiples = (multiples % input) + '';
+    }
+  }
+
+  console.log(res.substring(0, res.length - 1));
+  return res.substring(0, res.length - 1);
 };
 
 describe('1', () => {
   it('TC1', () => {
-    expect(solution(`5 8 4`)).toStrictEqual(`1
-1
-0
-0`);
+    expect(
+      solution(`3
+7
+9901`)
+    ).toStrictEqual(`3
+6
+12`);
   });
 });
