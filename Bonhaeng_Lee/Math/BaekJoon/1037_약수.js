a77692365d98878d482f6ecef0f9011ce36ac69a
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 03:56:20 +0900

    Solve: Math - BOJ - 1037 약수

--- Bonhaeng_Lee/Math/BaekJoon/1037_약수.js
+++ Bonhaeng_Lee/Math/BaekJoon/1037_약수.js
@@ -1,18 +1,34 @@
 const solution = function (i) {
-  const [A, B, C] = i.toString().trim().split(' ').map(Number);
-  const A_C = A % C;
-  const B_C = B % C;
-  console.log((A + B) % C);
-  console.log((A_C + B_C) % C);
-  console.log((A * B) % C);
-  console.log((A_C * B_C) % C);
+  const [N, ...divisor] = i.toString().trim().split(/\s/).map(Number);
+  const max = Math.max(...divisor);
+  const min = Math.min(...divisor);
+  console.log(max * min);
+  return max * min;
 };
 
 describe('약수', () => {
   it('TC1', () => {
-    expect(solution(`5 8 4`)).toStrictEqual(`1
-1
-0
-0`);
+    expect(
+      solution(`2
+4 2`)
+    ).toStrictEqual(8);
+  });
+  it('TC2', () => {
+    expect(
+      solution(`1
+2`)
+    ).toStrictEqual(4);
+  });
+  it('TC3', () => {
+    expect(
+      solution(`6
+3 4 2 12 6 8`)
+    ).toStrictEqual(24);
+  });
+  it('TC4', () => {
+    expect(
+      solution(`14
+14 26456 2 28 13228 3307 7 23149 8 6614 46298 56 4 92596`)
+    ).toStrictEqual(185192);
   });
 });
