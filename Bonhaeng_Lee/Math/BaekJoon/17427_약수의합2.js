d724d675a11efcd08f7c99aab017fa264d2ef9ce
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 04:44:10 +0900

    Solve: Math - BOJ - 17427 약수의 합 2

--- Bonhaeng_Lee/Math/BaekJoon/17427_약수의합2.js
+++ Bonhaeng_Lee/Math/BaekJoon/17427_약수의합2.js
@@ -1,18 +1,27 @@
 const solution = function (i) {
-  const [A, B, C] = i.toString().trim().split(' ').map(Number);
-  const A_C = A % C;
-  const B_C = B % C;
-  console.log((A + B) % C);
-  console.log((A_C + B_C) % C);
-  console.log((A * B) % C);
-  console.log((A_C * B_C) % C);
+  const N = +i.toString().trim();
+  let res = 0;
+  for (let i = 1; i <= N; i++) {
+    res += i * Math.floor(N / i);
+  }
+  console.log(res);
+  return res;
 };
 
 describe('약수의합2', () => {
-  it('TC1', () => {
-    expect(solution(`5 8 4`)).toStrictEqual(`1
-1
-0
-0`);
+  // it('TC1', () => {
+  //   expect(solution(`1`)).toStrictEqual(1);
+  // });
+  // it('TC2', () => {
+  //   expect(solution(`2`)).toStrictEqual(4);
+  // });
+  it('TC3', () => {
+    expect(solution(`10`)).toStrictEqual(87);
   });
+  // it('TC4', () => {
+  //   expect(solution(`70`)).toStrictEqual(4065);
+  // });
+  // it('TC5', () => {
+  //   expect(solution(`10000`)).toStrictEqual(82256014);
+  // });
 });
