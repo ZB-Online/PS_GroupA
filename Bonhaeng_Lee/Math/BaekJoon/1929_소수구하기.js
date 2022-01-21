a8ce866e3652824c777009f6c1c014dc57ab6cef
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 10:50:11 +0900

    Solve: Math - BOJ - 1929 소수구하기

--- Bonhaeng_Lee/Math/BaekJoon/1929_소수구하기.js
+++ Bonhaeng_Lee/Math/BaekJoon/1929_소수구하기.js
@@ -1,18 +1,32 @@
 const solution = function (i) {
-  const [A, B, C] = i.toString().trim().split(' ').map(Number);
-  const A_C = A % C;
-  const B_C = B % C;
-  console.log((A + B) % C);
-  console.log((A_C + B_C) % C);
-  console.log((A * B) % C);
-  console.log((A_C * B_C) % C);
+  const answer = [];
+  const nums = i.toString().split(' ').map(Number);
+
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+
+    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+      if (num % i === 0) return false;
+    }
+
+    return true;
+  }
+
+  for (let i = nums[0]; i <= nums[1]; i++) {
+    if (isPrime(i)) answer.push(i);
+  }
+  console.log(answer.join('\n'));
+  return answer.join('\n');
 };
 
 describe('소수구하기', () => {
   it('TC1', () => {
-    expect(solution(`5 8 4`)).toStrictEqual(`1
-1
-0
-0`);
+    expect(solution(`3 16`)).toStrictEqual(`3
+5
+7
+11
+13
+`);
   });
 });
