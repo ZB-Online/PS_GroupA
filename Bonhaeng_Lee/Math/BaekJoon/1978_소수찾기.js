b21701a1f34ff3ea0e7cdbfd37a2ae662c526692
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-21 10:50:35 +0900

    Solve: Math - BOJ - 1978 소수찾기

--- Bonhaeng_Lee/Math/BaekJoon/1978_소수찾기.js
+++ Bonhaeng_Lee/Math/BaekJoon/1978_소수찾기.js
@@ -1,18 +1,29 @@
 const solution = function (i) {
-  const [A, B, C] = i.toString().trim().split(' ').map(Number);
-  const A_C = A % C;
-  const B_C = B % C;
-  console.log((A + B) % C);
-  console.log((A_C + B_C) % C);
-  console.log((A * B) % C);
-  console.log((A_C * B_C) % C);
+  let answer = 0;
+  const nums = i.toString().split('\n')[1].split(' ').map(Number);
+
+  function isPrime(num) {
+    if (num <= 1) return false;
+    if (num === 2) return true;
+
+    for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++)
+      if (num % i === 0) return false;
+
+    return true;
+  }
+
+  for (const x of nums) {
+    if (isPrime(x)) answer++;
+  }
+  console.log(answer);
+  return answer;
 };
 
 describe('소수찾기', () => {
   it('TC1', () => {
-    expect(solution(`5 8 4`)).toStrictEqual(`1
-1
-0
-0`);
+    expect(
+      solution(`4
+1 3 5 7`)
+    ).toStrictEqual(3);
   });
 });
