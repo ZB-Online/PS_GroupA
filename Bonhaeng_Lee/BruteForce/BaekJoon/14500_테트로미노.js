93aaa7640cc092d1263fceba7ed2e9739cda6fa9
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-25 08:09:17 +0900

    Solve: BruteForce - BOJ - 14500. 테트로미노

--- Bonhaeng_Lee/BruteForce/BaekJoon/14500_테트로미노.js
+++ Bonhaeng_Lee/BruteForce/BaekJoon/14500_테트로미노.js
@@ -7,119 +7,78 @@ const solution = function (i) {
       i === 0 ? v.split(' ').map(Number) : v.split(' ').map(v => +v)
     );
 
-  // 회전
-  function rotate(degree, shape) {
-    const _shape = shape;
-    const shapeSize = _shape.length;
-
-    switch (degree) {
-      case 0:
-        break;
-      case 90:
-        for (let shpIdx = 0; shpIdx < shapeSize; shpIdx++)
-          _shape[shpIdx] = [
-            0 * _shape[shpIdx][0] + -1 * _shape[shpIdx][1],
-            1 * _shape[shpIdx][0] + 0 * _shape[shpIdx][1],
-          ];
-        break;
-      case 180:
-        for (let shpIdx = 0; shpIdx < shapeSize; shpIdx++)
-          _shape[shpIdx] = [
-            -1 * _shape[shpIdx][0] + 0 * _shape[shpIdx][1],
-            0 * _shape[shpIdx][0] + -1 * _shape[shpIdx][1],
-          ];
-        break;
-      case 270:
-        for (let shpIdx = 0; shpIdx < shapeSize; shpIdx++)
-          _shape[shpIdx] = [
-            0 * _shape[shpIdx][0] + 1 * _shape[shpIdx][1],
-            -1 * _shape[shpIdx][0] + 0 * _shape[shpIdx][1],
-          ];
-        break;
-      default:
-        console.log('error');
-        break;
+  let result = Number.MIN_SAFE_INTEGER;
+  const dx = [0, 0, -1, 1];
+  const dy = [-1, 1, 0, 0];
+  function DFS(y, x, cnt, sum, visit) {
+    if (cnt >= 4) {
+      result = Math.max(result, sum);
+      return;
     }
 
-    return _shape;
-  }
+    for (let k = 0; k < 4; k++) {
+      const ny = y + dy[k];
+      const nx = x + dx[k];
 
-  // 뒤집기
-  function flip(shape) {
-    const _shape = shape;
-    const shapeSize = _shape.length;
-
-    for (let shpIdx = 0; shpIdx < shapeSize; shpIdx++)
-      _shape[shpIdx] = [-_shape[shpIdx][0], _shape[shpIdx][1]];
-
-    return _shape;
-  }
-
-  // 합
-  let answer = -1;
-  function shapeSum(shape, row, col) {
-    let sum = matrix[row][col];
-    const shapeSize = shape.length;
-
-    for (let shpIdx = 0; shpIdx < shapeSize; shpIdx++) {
-      const nowRow = row + shape[shpIdx][0];
-      const nowCol = col + shape[shpIdx][1];
-
-      if (nowRow < 0 || nowRow > N - 1) return;
-      if (nowCol < 0 || nowCol > M - 1) return;
-      sum += matrix[nowRow][nowCol];
-    }
-
-    if (sum > answer) answer = sum;
-  }
-
-  const shapeVec = [
-    [
-      [0, 1],
-      [0, 2],
-      [0, 3],
-    ],
-    [
-      [0, 1],
-      [1, 0],
-      [1, 1],
-    ],
-    [
-      [1, 0],
-      [2, 0],
-      [2, 1],
-    ],
-    [
-      [1, 0],
-      [1, 1],
-      [2, 1],
-    ],
-    [
-      [0, 1],
-      [0, 2],
-      [1, 1],
-    ],
-  ];
-
-  for (let rowIdx = 0; rowIdx < N; rowIdx++) {
-    for (let colIdx = 0; colIdx < M; colIdx++) {
-      const shapeVecSize = shapeVec.length;
-      for (let shpIdx = 0; shpIdx < shapeVecSize; shpIdx++) {
-        for (let chgIdx = 0; chgIdx < 4; chgIdx++) {
-          const _shape = rotate(chgIdx * 90, shapeVec[shpIdx]);
-          shapeSum(_shape, rowIdx, colIdx);
-        }
-        for (let chgIdx = 0; chgIdx < 4; chgIdx++) {
-          let _shape = flip(shapeVec[shpIdx]);
-          _shape = rotate(chgIdx * 90, _shape);
-          shapeSum(_shape, rowIdx, colIdx);
-        }
+      if (!(ny < 0 || nx < 0 || ny >= N || nx >= M || visit[ny][nx])) {
+        visit[ny][nx] = true;
+        DFS(ny, nx, cnt + 1, sum + matrix[ny][nx], visit);
+        visit[ny][nx] = false;
       }
     }
   }
 
-  console.log(answer);
-  return answer;
+  function checkEdgeCaseSum(y, x) {
+    // ㅏ
+    if (y + 2 < N && x + 1 < M)
+      result = Math.max(
+        result,
+        matrix[y][x] +
+          matrix[y + 1][x] +
+          matrix[y + 2][x] +
+          matrix[y + 1][x + 1]
+      );
+    // ㅓ
+    if (y + 2 < N && x - 1 > 0)
+      result = Math.max(
+        result,
+        matrix[y][x] +
+          matrix[y + 1][x] +
+          matrix[y + 2][x] +
+          matrix[y + 1][x - 1]
+      );
+    // ㅗ
+    if (y + 1 < N && x + 2 < M)
+      result = Math.max(
+        result,
+        matrix[y][x] +
+          matrix[y][x + 1] +
+          matrix[y][x + 2] +
+          matrix[y + 1][x + 1]
+      );
+    // ㅜ
+    if (y > 0 && x + 2 < M)
+      result = Math.max(
+        result,
+        matrix[y][x] +
+          matrix[y][x + 1] +
+          matrix[y][x + 2] +
+          matrix[y - 1][x + 1]
+      );
+  }
+
+  const visit = new Array(N).fill(false).map(() => new Array(M).fill(false));
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      visit[i][j] = true;
+      DFS(i, j, 1, matrix[i][j], visit);
+      visit[i][j] = false;
+      checkEdgeCaseSum(i, j);
+    }
+  }
+
+  console.log(result);
+  return result;
 };
 
 describe('테트로미노', () => {
