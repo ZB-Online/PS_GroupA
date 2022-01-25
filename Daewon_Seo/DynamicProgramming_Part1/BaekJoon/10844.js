683e2bb42365682d9bef600e097012f8942aea6f
Serzhul <wilshere917@gmail.com>
2022-01-25 19:32:41 +0900

    Problem set setting

---
+++ Daewon_Seo/DynamicProgramming_Part1/BaekJoon/10844.js
@@ -0,0 +1,8 @@
+const solution = (input) => {
+    const [N, ...candies] = input
+        .toString()
+        .trim()
+        .split('\n')
+        .map((el, i) => (i === 0 ? +el : el.split('')));
+};
+solution(``);
