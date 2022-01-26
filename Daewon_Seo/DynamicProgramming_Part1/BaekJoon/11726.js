22566f8a479e05aa1ae04bec8993d9447396656f
Serzhul <wilshere917@gmail.com>
2022-01-26 18:46:01 +0900

    11726 solved

--- Daewon_Seo/DynamicProgramming_Part1/BaekJoon/11726.js
+++ Daewon_Seo/DynamicProgramming_Part1/BaekJoon/11726.js
@@ -1,8 +1,14 @@
 const solution = (input) => {
-    const [N, ...candies] = input
-        .toString()
-        .trim()
-        .split('\n')
-        .map((el, i) => (i === 0 ? +el : el.split('')));
+    const N = input.toString().trim();
+
+    const dp = Array.from({ length: 1001 }, () => 0);
+
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (let i = 3; i <= N; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+    }
+    console.log(dp[N]);
 };
-solution(``);
+solution(`4`);
