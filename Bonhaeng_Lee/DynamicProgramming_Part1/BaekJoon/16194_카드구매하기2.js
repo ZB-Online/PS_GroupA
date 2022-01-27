01f60ba9d8991d55c4e3364915f2642154effa54
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-28 08:46:57 +0900

    Solve: DP_part1 - 16194. 카드 구매하기2

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/16194_카드구매하기2.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/16194_카드구매하기2.js
@@ -1,21 +1,52 @@
 const solution = function (i) {
-  const TC = i
-    .toString()
-    .trim()
-    .split('\n')
-    .map(v => v.split(' ').map(Number));
-  TC.shift();
+  const [N, ...Pack] = i.toString().trim().split(/\s/).map(Number);
+
+  const DP = [0, ...Pack];
+  for (let i = 2; i <= N; i++) {
+    // 범위 제한으로 중복 연산 제거
+    for (let j = 1; j <= i / 2; j++) {
+      DP[i] = Math.min(DP[i], DP[j] + DP[i - j]);
+    }
+  }
+  console.log(DP[N]);
+  return DP[N];
 };
 
 describe('카드 구매하기 2', () => {
   it('TC1', () => {
     expect(
-      solution(`3
-10 12 3 9
-10 12 7 2
-13 11 5 6`)
-    ).toStrictEqual(`33
--1
-83`);
+      solution(`4
+1 5 6 7`)
+    ).toStrictEqual(4);
+  });
+  it('TC2', () => {
+    expect(
+      solution(`5
+10 9 8 7 6`)
+    ).toStrictEqual(6);
+  });
+  it('TC3', () => {
+    expect(
+      solution(`10
+1 1 2 3 5 8 13 21 34 55`)
+    ).toStrictEqual(5);
+  });
+  it('TC4', () => {
+    expect(
+      solution(`10
+5 10 11 12 13 30 35 40 45 47`)
+    ).toStrictEqual(26);
+  });
+  it('TC5', () => {
+    expect(
+      solution(`4
+5 2 8 10`)
+    ).toStrictEqual(4);
+  });
+  it('TC6', () => {
+    expect(
+      solution(`4
+3 5 15 16`)
+    ).toStrictEqual(10);
   });
 });
