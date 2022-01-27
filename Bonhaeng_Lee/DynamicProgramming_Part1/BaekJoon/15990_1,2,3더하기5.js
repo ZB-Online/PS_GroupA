3c52d00e61cfa15d0be964d79459665f94e9a0b7
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-28 08:47:12 +0900

    Solve: DP_part1 - 15990. 1,2,3 더하기 5

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/15990_1,2,3더하기5.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/15990_1,2,3더하기5.js
@@ -1,40 +1,40 @@
 const solution = function (i) {
-  const [[N, M], ...matrix] = i
-    .toString()
-    .trim()
-    .split('\n')
-    .map((v, i) =>
-      i === 0 ? v.split(' ').map(Number) : v.split(' ').map(v => +v)
-    );
+  const [T, ...tcArr] = i.toString().trim().split('\n').map(Number);
+  let result = '';
+  // 시간 초과 방지
+  const max = Math.max(...tcArr);
+
+  const DP = Array.from({ length: max + 1 }, () =>
+    Array.from({ length: 4 }, () => 0)
+  );
+  DP[1][1] = 1;
+  DP[2][2] = 1;
+  DP[3][1] = 1;
+  DP[3][2] = 1;
+  DP[3][3] = 1;
+  for (let i = 4; i <= max; i++) {
+    DP[i][1] = (DP[i - 1][2] + DP[i - 1][3]) % 1000000009;
+    DP[i][2] = (DP[i - 2][1] + DP[i - 2][3]) % 1000000009;
+    DP[i][3] = (DP[i - 3][1] + DP[i - 3][2]) % 1000000009;
+  }
+
+  for (const tc of tcArr) {
+    result += ((DP[tc][1] + DP[tc][2] + DP[tc][3]) % 1000000009) + '\n';
+  }
+
+  console.log(result.substring(0, result.length - 1));
+  return result.substring(0, result.length - 1);
 };
 
 describe('1,2,3 더하기 5', () => {
   it('TC1', () => {
     expect(
-      solution(`5 5
-1 2 3 4 5
-5 4 3 2 1
-2 3 4 5 6
-6 5 4 3 2
-1 2 1 2 1`)
-    ).toStrictEqual(19);
-  });
-  it('TC2', () => {
-    expect(
-      solution(`4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5`)
-    ).toStrictEqual(20);
-  });
-  it('TC3', () => {
-    expect(
-      solution(`4 10
-1 2 1 2 1 2 1 2 1 2
-2 1 2 1 2 1 2 1 2 1
-1 2 1 2 1 2 1 2 1 2
-2 1 2 1 2 1 2 1 2 1`)
-    ).toStrictEqual(7);
+      solution(`3
+4
+7
+10`)
+    ).toStrictEqual(`3
+9
+27`);
   });
 });
