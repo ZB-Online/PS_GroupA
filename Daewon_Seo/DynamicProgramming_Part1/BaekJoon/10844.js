14a26e2a325439e3934ee81cdfa4d4c2ea8b5884
Serzhul <wilshere917@gmail.com>
2022-01-27 22:46:33 +0900

    15990 solved

--- Daewon_Seo/DynamicProgramming_Part1/BaekJoon/10844.js
+++ Daewon_Seo/DynamicProgramming_Part1/BaekJoon/10844.js
@@ -1,8 +1,16 @@
 const solution = (input) => {
-    const [N, ...candies] = input
-        .toString()
-        .trim()
-        .split('\n')
-        .map((el, i) => (i === 0 ? +el : el.split('')));
+    const N = input.toString().trim();
+
+    const num = +N;
+
+    const dp = [0];
+
+    dp[1] = 9;
+
+    for (let i = 2; i <= N; i++) {
+        dp[i] = dp[i - 1] + 1 + (9 - i - 1) * 2;
+    }
+
+    console.log(dp[N]);
 };
-solution(``);
+solution(`3`);
