6a1d06d456873bfa9029215fbd332bebdea09407
BonhaengLee <bon952014@ajou.ac.kr>
2022-01-28 09:28:47 +0900

    Solve: DP_part1 - 10844. 쉬운 계단 수

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/10844_쉬운계단수.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/10844_쉬운계단수.js
@@ -1,40 +1,36 @@
 const solution = function (i) {
-  const [[N, M], ...matrix] = i
-    .toString()
-    .trim()
-    .split('\n')
-    .map((v, i) =>
-      i === 0 ? v.split(' ').map(Number) : v.split(' ').map(v => +v)
-    );
+  const N = i.toString().trim() * 1;
+  // 행 : 1 ~ N 자릿수
+  // 열 : 0 ~ 9 앞에 올 수 있는 숫자
+  // 값 : 경우의 수
+  const DP = Array(N + 1)
+    .fill(0)
+    .map(() => Array(10).fill(0));
+  for (let i = 1; i <= 9; i++) {
+    DP[1][i] = 1;
+  }
+
+  for (let i = 2; i <= N; i++) {
+    for (let j = 0; j <= 9; j++) {
+      if (j === 0) {
+        DP[i][j] = DP[i - 1][1] % 1000000000;
+      } else if (j === 9) {
+        DP[i][j] = DP[i - 1][8] % 1000000000;
+      } else {
+        DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
+      }
+    }
+  }
+  const result = DP[N].reduce((a, b) => a + b, 0) % 1000000000;
+  console.log(result);
+  return result;
 };
 
 describe('쉬운계단수', () => {
   it('TC1', () => {
-    expect(
-      solution(`5 5
-1 2 3 4 5
-5 4 3 2 1
-2 3 4 5 6
-6 5 4 3 2
-1 2 1 2 1`)
-    ).toStrictEqual(19);
+    expect(solution(`1`)).toStrictEqual(9);
   });
   it('TC2', () => {
-    expect(
-      solution(`4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5`)
-    ).toStrictEqual(20);
-  });
-  it('TC3', () => {
-    expect(
-      solution(`4 10
-1 2 1 2 1 2 1 2 1 2
-2 1 2 1 2 1 2 1 2 1
-1 2 1 2 1 2 1 2 1 2
-2 1 2 1 2 1 2 1 2 1`)
-    ).toStrictEqual(7);
+    expect(solution(`2`)).toStrictEqual(17);
   });
 });
