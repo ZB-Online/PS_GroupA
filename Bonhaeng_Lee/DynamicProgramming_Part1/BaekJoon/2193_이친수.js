0bdc6861f9c187503a1a2b98b7cbe67655c2498b
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-09 03:18:25 +0900

    Solve: DP_part1 - 2193. 이친수

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/2193_이친수.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/2193_이친수.js
@@ -1,29 +1,18 @@
 const solution = function (i) {
   const N = i.toString().trim() * 1;
-  const DP = Array(N + 1)
-    .fill(0)
-    .map(() => Array(10).fill(0));
-  for (let i = 1; i <= 9; i++) {
-    DP[1][i] = 1;
-  }
+  const DP = Array(N + 1).fill(1);
 
+  DP[0] = 1;
+  DP[1] = 1;
   for (let i = 2; i <= N; i++) {
-    for (let j = 0; j <= 9; j++) {
-      if (j === 0) {
-        DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
-      }
-    }
+    DP[i] = BigInt(DP[i - 2]) + BigInt(DP[i - 1]);
   }
-  const result = DP[N].reduce((a, b) => a + b, 0) % 1000000000;
-  console.log(result);
-  return result;
+  console.log(DP[N - 1].toString());
+  return DP[N - 1].toString();
 };
 
-describe('연속합', () => {
+describe('이친수', () => {
   it('TC1', () => {
-    expect(solution(`1`)).toStrictEqual(9);
-  });
-  it('TC2', () => {
-    expect(solution(`2`)).toStrictEqual(17);
+    expect(solution(`3`)).toStrictEqual('2');
   });
 });
