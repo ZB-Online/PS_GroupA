12e26d07b4186dbb3f62d3ae52840e5bd29e7792
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-09 01:39:01 +0900

    Solve: DP_part1 - 1699. 제곱수의합

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/1699_제곱수의합.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/1699_제곱수의합.js
@@ -1,29 +1,38 @@
 const solution = function (i) {
   const N = i.toString().trim() * 1;
-  const DP = Array(N + 1)
-    .fill(0)
-    .map(() => Array(10).fill(0));
-  for (let i = 1; i <= 9; i++) {
-    DP[1][i] = 1;
-  }
+  const DP = Array(N + 1).fill(0);
+  DP[1] = 1;
 
   for (let i = 2; i <= N; i++) {
-    for (let j = 0; j <= 9; j++) {
-      if (j === 0) {
-        DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
-      }
+    let min = Number.MAX_SAFE_INTEGER;
+
+    for (let j = 1; j <= Math.floor(i / 2); j++) {
+      if (j * j === i) {
+        min = 1;
+        break;
+      } else min = Math.min(min, DP[j] + DP[i - j]);
     }
+
+    DP[i] = min;
   }
-  const result = DP[N].reduce((a, b) => a + b, 0) % 1000000000;
-  console.log(result);
-  return result;
+  console.log(DP[N]);
+  return DP[N];
 };
 
-describe('연속합', () => {
+describe('제곱수의합', () => {
   it('TC1', () => {
-    expect(solution(`1`)).toStrictEqual(9);
+    expect(solution(`7`)).toStrictEqual(4);
   });
   it('TC2', () => {
-    expect(solution(`2`)).toStrictEqual(17);
+    expect(solution(`1`)).toStrictEqual(1);
+  });
+  it('TC3', () => {
+    expect(solution(`4`)).toStrictEqual(1);
+  });
+  it('TC4', () => {
+    expect(solution(`11`)).toStrictEqual(3);
+  });
+  it('TC5', () => {
+    expect(solution(`13`)).toStrictEqual(2);
   });
 });
