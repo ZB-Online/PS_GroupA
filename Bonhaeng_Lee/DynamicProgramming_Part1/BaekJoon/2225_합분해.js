e1cc36b3868832fbc614187d71284db4392cc4c0
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-09 02:23:25 +0900

    Solve: DP_part1 - 2225. 합분해

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/2225_합분해.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/2225_합분해.js
@@ -1,29 +1,30 @@
 const solution = function (i) {
-  const N = i.toString().trim() * 1;
-  const DP = Array(N + 1)
-    .fill(0)
-    .map(() => Array(10).fill(0));
-  for (let i = 1; i <= 9; i++) {
-    DP[1][i] = 1;
+  const [N, K] = i.toString().trim().split(' ').map(Number);
+  const DP = Array(K + 1)
+    .fill(1)
+    .map(() => Array(N + 1).fill(1));
+
+  for (let i = 1; i <= K; i++) {
+    DP[i][0] = 1;
   }
 
-  for (let i = 2; i <= N; i++) {
-    for (let j = 0; j <= 9; j++) {
-      if (j === 0) {
-        DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
-      }
+  const DIV = 1_000_000_000;
+  for (let i = 2; i <= K; i++) {
+    for (let j = 1; j <= N; j++) {
+      DP[i][j] = DP[i - 1][j] + DP[i][j - 1];
+      DP[i][j] %= DIV;
     }
   }
-  const result = DP[N].reduce((a, b) => a + b, 0) % 1000000000;
-  console.log(result);
-  return result;
+
+  console.log(DP[K][N]);
+  return DP[K][N];
 };
 
-describe('연속합', () => {
+describe('합분해', () => {
   it('TC1', () => {
-    expect(solution(`1`)).toStrictEqual(9);
+    expect(solution(`20 2`)).toStrictEqual(21);
   });
   it('TC2', () => {
-    expect(solution(`2`)).toStrictEqual(17);
+    expect(solution(`6 4`)).toStrictEqual(84);
   });
 });
