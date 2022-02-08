8386cbeaec97bdf280bf61875dd903f418e76fd1
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-09 03:06:45 +0900

    Solve: DP_part1 - 14002. 가장 긴 증가하는 부분수열 4

--- Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/14002_가장긴증가하는부분수열4.js
+++ Bonhaeng_Lee/DynamicProgramming_Part1/BaekJoon/14002_가장긴증가하는부분수열4.js
@@ -1,29 +1,43 @@
 const solution = function (i) {
-  const N = i.toString().trim() * 1;
-  const DP = Array(N + 1)
-    .fill(0)
-    .map(() => Array(10).fill(0));
-  for (let i = 1; i <= 9; i++) {
-    DP[1][i] = 1;
-  }
+  const [N, ...A] = i.toString().trim().split(/\s+/).map(Number);
+  const DP = Array(N + 1).fill(1);
 
-  for (let i = 2; i <= N; i++) {
-    for (let j = 0; j <= 9; j++) {
-      if (j === 0) {
-        DP[i][j] = (DP[i - 1][j - 1] + DP[i - 1][j + 1]) % 1000000000;
+  const indices = Array(N + 1).fill(0);
+
+  let maxIdx = 0;
+  let max = 0;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < i; j++) {
+      if (A[j] < A[i] && DP[j] + 1 > DP[i]) {
+        DP[i] = DP[j] + 1;
+        indices[i] = j;
       }
     }
+
+    if (DP[i] > max) {
+      max = DP[i];
+      maxIdx = i;
+    }
   }
-  const result = DP[N].reduce((a, b) => a + b, 0) % 1000000000;
-  console.log(result);
-  return result;
+
+  let index = maxIdx;
+  const ans = [];
+  // maxIdx부터 부분 수열의 이전 인덱스를 찾아감, 50(5) -> 30(3) -> 20(1) -> 10(0)
+  for (let i = max - 1; i >= 0; i--) {
+    ans[i] = A[index];
+    index = indices[index];
+  }
+  console.log(max + '\n' + ans.join(' '));
+
+  return max + '\n' + ans.join(' ');
 };
 
-describe('연속합', () => {
+describe('가장 긴 증가하는 부분수열 4', () => {
   it('TC1', () => {
-    expect(solution(`1`)).toStrictEqual(9);
-  });
-  it('TC2', () => {
-    expect(solution(`2`)).toStrictEqual(17);
+    expect(
+      solution(`6
+10 20 10 30 20 50`)
+    ).toStrictEqual(`4
+10 20 30 50`);
   });
 });
