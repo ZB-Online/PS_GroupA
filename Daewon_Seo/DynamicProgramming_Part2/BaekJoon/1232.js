802c2e0bb9ce8bbd80d09461da9f110ab46ed425
Serzhul <wilshere917@gmail.com>
2022-02-09 20:31:02 +0900

    1232 solved

--- Daewon_Seo/DynamicProgramming_Part2/BaekJoon/1232.js
+++ Daewon_Seo/DynamicProgramming_Part2/BaekJoon/1232.js
@@ -1,9 +1,28 @@
 const solution = (input) => {
-    const [n, nums] = input
+    const [n, ...nums] = input
         .toString()
         .trim()
         .split('\n')
-        .map((el, i) => (i === 0 ? +el : el.split(' ').map((num) => +num)));
+        .map((el) => +el);
+
+    const dp = Array.from({ length: 1000001 });
+
+    const mod = 1000000009;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for (let i = 4; i <= 1000000; i += 1) {
+        dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % mod;
+    }
+
+    for (let i = 0; i < n; i += 1) {
+        console.log(dp[nums[i]]);
+    }
 };
 
-solution(``);
+solution(`3
+4
+7
+10`);
