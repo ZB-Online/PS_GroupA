2bef7ced54cbff198b3ccd7673e72609b6719990
Serzhul <wilshere917@gmail.com>
2022-02-11 09:59:12 +0900

    2156 solved

--- Daewon_Seo/DynamicProgramming_Part2/BaekJoon/15988.js
+++ Daewon_Seo/DynamicProgramming_Part2/BaekJoon/15988.js
@@ -1,9 +1,22 @@
-const solution = (input) => {
-    const [n, nums] = input
-        .toString()
-        .trim()
-        .split('\n')
-        .map((el, i) => (i === 0 ? +el : el.split(' ').map((num) => +num)));
-};
+const [n, ...nums] = require('fs')
+    .readFileSync('/dev/stdin')
+    .toString()
+    .trim()
+    .split('\n')
+    .map((el) => +el);
 
-solution(``);
+const dp = Array.from({ length: 1000001 });
+
+const mod = 1000000009;
+
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+
+for (let i = 4; i <= 1000000; i += 1) {
+    dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % mod;
+}
+
+for (let i = 0; i < n; i += 1) {
+    console.log(dp[nums[i]]);
+}
--- Daewon_Seo/DynamicProgramming_Part2/BaekJoon/2156.js
+++ Daewon_Seo/DynamicProgramming_Part2/BaekJoon/2156.js
@@ -5,16 +5,19 @@ const solution = (input) => {
         .split('\n')
         .map((el) => +el);
 
-    const dp = Array(n);
+    const dp = Array(n).fill(0);
 
-    dp[0] = nums[0];
-    dp[1] = nums[0] + nums[1];
-    dp[2] = nums[0] + nums[2];
+    dp[1] = nums[0];
+    dp[2] = nums[0] + nums[1];
 
-    for (let i = 3; i < n; i += 1) {
-        dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+    for (let i = 3; i <= n; i += 1) {
+        dp[i] = Math.max(
+            dp[i - 3] + nums[i - 2] + nums[i - 1],
+            dp[i - 2] + nums[i - 1],
+            dp[i - 1]
+        );
     }
-    // console.log(dp);
+    console.log(dp[n]);
 };
 
 solution(`6
